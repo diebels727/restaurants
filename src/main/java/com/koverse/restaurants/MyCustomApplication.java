1cb9eea6522fad6e795cba0e75f91701e209427a
diebels727 <diebelsalternative@hotmail.com>
2015-06-24 11:47:08 -0400

    Change application ID

--- src/main/java/com/koverse/restaurants/MyCustomApplication.java
+++ src/main/java/com/koverse/restaurants/MyCustomApplication.java
@@ -6,58 +6,58 @@ import java.util.List;
 
 public class MyCustomApplication extends AbstractApplication {
 
-	/**
-	 * A universally unique string that indentifies this application. This
-	 * string must be the same across all versions of the application.
-	 */
-	@Override
-	public String getApplicationId() {
-		return "myCustomApplication";
-	}
+  /**
+   * A universally unique string that indentifies this application. This
+   * string must be the same across all versions of the application.
+   */
+  @Override
+  public String getApplicationId() {
+    return "restaurants";
+  }
 
-	/** The application name which is shown to the users. */
-	@Override
-	public String getDisplayName() {
-		return "My Custom Application";
-	}
+  /** The application name which is shown to the users. */
+  @Override
+  public String getDisplayName() {
+    return "Restaurant Inspection Violations";
+  }
 
-	/**
-	 * The name of the category in which this application is grouped in the UI.
-	 * The name much match the desired group name exactly.
-	 */
-	@Override
-	public String getDefaultCategoryName() {
-		return "My Custom Category";
-	}
+  /**
+   * The name of the category in which this application is grouped in the UI.
+   * The name much match the desired group name exactly.
+   */
+  @Override
+  public String getDefaultCategoryName() {
+    return "My Custom Category";
+  }
 
-	/**
-	 * A string that simply identifies the version of the application installed,
-	 * but which has no bearing on version control. The last installed version
-	 * of the app is the one that is used.
-	 */
-	@Override
-	public String getVersion() {
-		return "0.0.1";
-	}
+  /**
+   * A string that simply identifies the version of the application installed,
+   * but which has no bearing on version control. The last installed version
+   * of the app is the one that is used.
+   */
+  @Override
+  public String getVersion() {
+    return "0.0.1";
+  }
 
-	/** The parameters that a system administrator can use to configure the app. */
-	@Override
-	protected void fillInParameters(List<Parameter> parameters) {
+  /** The parameters that a system administrator can use to configure the app. */
+  @Override
+  protected void fillInParameters(List<Parameter> parameters) {
 
-//		parameters.add(new Parameter.Builder()
-//			.parameterName("testParameter")
-//			.displayName("Test Parameter").type(Parameter.TYPE_STRING)
-//			.defaultValue("default value").build());
+//    parameters.add(new Parameter.Builder()
+//      .parameterName("testParameter")
+//      .displayName("Test Parameter").type(Parameter.TYPE_STRING)
+//      .defaultValue("default value").build());
 
-	}
+  }
 
-	/**
-	 * Set this false to force the system administrator to manually deploy
-	 * instances of this application. Usually enabling auto-deploy is the most
-	 * appropriate.
-	 */
-	@Override
-	public boolean getAutoDeploy() {
-		return true;
-	}
+  /**
+   * Set this false to force the system administrator to manually deploy
+   * instances of this application. Usually enabling auto-deploy is the most
+   * appropriate.
+   */
+  @Override
+  public boolean getAutoDeploy() {
+    return true;
+  }
 }
