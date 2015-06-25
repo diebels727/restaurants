a7b14846656dd7f10523dd6e871a4909bbda7324
diebels727 <diebelsalternative@hotmail.com>
2015-06-24 20:11:57 -0400

    Load multiple markers.

--- src/main/resources/apps/restaurants/js/index.js
+++ src/main/resources/apps/restaurants/js/index.js
@@ -1,38 +1,52 @@
 /** This method will be called when JQuery is ready for use */
-$(function() {
+// $(function() {
+//
+//
+//   // Listen for the koverse JS to be ready
+//   Koverse.addEventListener(KoverseApplication.EVENT_READY, function() {
+//
+//     // Example koverse call - retrieve data collections
+//     Koverse.getDataCollections(function(response){
+//
+//       // Check that the response was successful
+//       if(!response.success) {
+//         $(document.body).append("<span class='error'>Error retrieving data collections: "  + response.failureMessage + "</span>");
+//         return;
+//       }
+//
+//       // sort the collections
+//       var dataCollectionsSorted = response.dataCollections.sortBy("name");
+//
+//       // Loop over the data collections in response
+//       $.each(dataCollectionsSorted, function(index, dataCollection){
+//
+//         // Append to list of data collections
+//         $("#dataCollectionList").append("<li><a href='" + KoverseUtil.getDataCollectionURL(dataCollection.id) + "'>" + dataCollection.name + "</a></li>");
+//
+//       });
+//
+//     });
+//
+//   });
+//
+// });
 
-
-  // Listen for the koverse JS to be ready
-  Koverse.addEventListener(KoverseApplication.EVENT_READY, function() {
-
-    // Example koverse call - retrieve data collections
-    Koverse.getDataCollections(function(response){
-
-      // Check that the response was successful
-      if(!response.success) {
-        $(document.body).append("<span class='error'>Error retrieving data collections: "  + response.failureMessage + "</span>");
-        return;
-      }
-
-      // sort the collections
-      var dataCollectionsSorted = response.dataCollections.sortBy("name");
-
-      // Loop over the data collections in response
-      $.each(dataCollectionsSorted, function(index, dataCollection){
-
-        // Append to list of data collections
-        $("#dataCollectionList").append("<li><a href='" + KoverseUtil.getDataCollectionURL(dataCollection.id) + "'>" + dataCollection.name + "</a></li>");
-
-      });
-
-    });
-
-  });
-
-});
 
 var app = angular.module('restApp',['uiGmapgoogle-maps']);
 
-app.controller('restAppController',['$scope',function($scope) {
-  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+app.controller('appController',[]);
+
+app.controller('mapController',['$scope',function($scope) {
+  $scope.markers = [
+    {id: 41670166,coords: {longitude: -73.97387341,latitude: 40.68614555},icon: 'images/blue_marker.png' },
+    {id: 41670166,coords: {longitude: -72.97387341,latitude: 41.68614555},icon: 'images/blue_marker.png' },
+    {id: 41670151,coords: {longitude: -73.98124063,latitude: 40.76397698},icon: 'images/blue_marker.png' }
+  ];
+
+
+  $scope.map = { center: { latitude: 40.7903, longitude: -73.9597 }, zoom: 10 };
+}]);
+
+app.controller('markerController',['$scope',function($scope) {
+
 }]);
