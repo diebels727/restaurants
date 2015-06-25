c456caa7a78d35e741ebe48c70688fb54d46631f
diebels727 <diebelsalternative@hotmail.com>
2015-06-24 20:24:54 -0400

    Add browser debugger.

--- src/main/resources/apps/restaurants/js/index.js
+++ src/main/resources/apps/restaurants/js/index.js
@@ -1,40 +1,38 @@
 /** This method will be called when JQuery is ready for use */
-// $(function() {
-//
-//
-//   // Listen for the koverse JS to be ready
-//   Koverse.addEventListener(KoverseApplication.EVENT_READY, function() {
-//
-//     // Example koverse call - retrieve data collections
-//     Koverse.getDataCollections(function(response){
-//
-//       // Check that the response was successful
-//       if(!response.success) {
-//         $(document.body).append("<span class='error'>Error retrieving data collections: "  + response.failureMessage + "</span>");
-//         return;
-//       }
-//
-//       // sort the collections
-//       var dataCollectionsSorted = response.dataCollections.sortBy("name");
-//
-//       // Loop over the data collections in response
-//       $.each(dataCollectionsSorted, function(index, dataCollection){
-//
-//         // Append to list of data collections
-//         $("#dataCollectionList").append("<li><a href='" + KoverseUtil.getDataCollectionURL(dataCollection.id) + "'>" + dataCollection.name + "</a></li>");
-//
-//       });
-//
-//     });
-//
-//   });
-//
-// });
+$(function() {
+
+
+  // Listen for the koverse JS to be ready
+  Koverse.addEventListener(KoverseApplication.EVENT_READY, function() {
+
+    // Example koverse call - retrieve data collections
+    Koverse.getDataCollections(function(response){
+
+      // Check that the response was successful
+      if(!response.success) {
+        $(document.body).append("<span class='error'>Error retrieving data collections: "  + response.failureMessage + "</span>");
+        return;
+      }
+
+      // sort the collections
+      var dataCollectionsSorted = response.dataCollections.sortBy("name");
+
+      // Loop over the data collections in response
+      $.each(dataCollectionsSorted, function(index, dataCollection){
+
+        // Append to list of data collections
+        $("#dataCollectionList").append("<li><a href='" + KoverseUtil.getDataCollectionURL(dataCollection.id) + "'>" + dataCollection.name + "</a></li>");
+
+      });
+
+    });
+
+  });
+
+});
 
 
 var app = angular.module('restApp',['uiGmapgoogle-maps']);
-
-app.controller('appController',[]);
 
 app.controller('mapController',['$scope',function($scope) {
   $scope.markers = [
@@ -43,6 +41,9 @@ app.controller('mapController',['$scope',function($scope) {
     {id: 41670151,coords: {longitude: -73.98124063,latitude: 40.76397698},icon: 'images/blue_marker.png' }
   ];
 
+  $scope.enterDebug = function() {
+    debugger;
+  }
 
   $scope.map = { center: { latitude: 40.7903, longitude: -73.9597 }, zoom: 10 };
 }]);
