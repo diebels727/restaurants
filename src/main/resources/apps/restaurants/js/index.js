0cfffb553df7e2e6775b0be3c8a2f946c6afac5f
diebels727 <diebelsalternative@hotmail.com>
2015-06-30 15:55:06 -0400

    Remove angular; basic gmap

--- src/main/resources/apps/restaurants/js/index.js
+++ src/main/resources/apps/restaurants/js/index.js
@@ -1,83 +1,49 @@
 /** This method will be called when JQuery is ready for use */
-$(function() {
 
+// $(function() {
+//
+//
+//   // Listen for the koverse JS to be ready
+//   Koverse.addEventListener(KoverseApplication.EVENT_READY, function() {
+//
+//     // Example koverse call - retrieve data collections
+//     Koverse.getDataCollections(function(response){
+//
+//       // Check that the response was successful
+//       if(!response.success) {
+//         $(document.body).append("<span class='error'>Error retrieving data collections: "  + response.failureMessage + "</span>");
+//         return;
+//       }
+//
+//       // sort the collections
+//       var dataCollectionsSorted = response.dataCollections.sortBy("name");
+//
+//       // Loop over the data collections in response
+//       $.each(dataCollectionsSorted, function(index, dataCollection){
+//
+//         // Append to list of data collections
+//         $("#dataCollectionList").append("<li><a href='" + KoverseUtil.getDataCollectionURL(dataCollection.id) + "'>" + dataCollection.name + "</a></li>");
+//
+//       });
+//
+//     });
+//
+//   });
+//
+// });
 
-  // Listen for the koverse JS to be ready
-  Koverse.addEventListener(KoverseApplication.EVENT_READY, function() {
+function enterDebug() {
+  debugger;
+};
 
-    // Example koverse call - retrieve data collections
-    Koverse.getDataCollections(function(response){
-
-      // Check that the response was successful
-      if(!response.success) {
-        $(document.body).append("<span class='error'>Error retrieving data collections: "  + response.failureMessage + "</span>");
-        return;
-      }
-
-      // sort the collections
-      var dataCollectionsSorted = response.dataCollections.sortBy("name");
-
-      // Loop over the data collections in response
-      $.each(dataCollectionsSorted, function(index, dataCollection){
-
-        // Append to list of data collections
-        $("#dataCollectionList").append("<li><a href='" + KoverseUtil.getDataCollectionURL(dataCollection.id) + "'>" + dataCollection.name + "</a></li>");
-
-      });
-
-    });
-
-  });
-
-});
-
-
-var app = angular.module('restApp',['uiGmapgoogle-maps']);
-
-app.controller('mapController',['$scope',function($scope) {
-  $scope.markers = [];
-
-  $scope.enterDebug = function() {
-    debugger;
-  }
-
-  $scope.loadRestaurantLocations = function(numberOfRestaurants) {
-    $scope.$apply(function(){
-
-      Koverse.getDataCollectionByName('geocoded-restaurants',function(dataCollectionResponse) {
-        $scope.dataCollection = dataCollectionResponse.dataCollection;
-
-        Koverse.performQuery("{$any: *}",[$scope.dataCollection.id],function(response) {
-          if ( !response.success) { debugger };
-          $scope.markersPrevLength = $scope.markers.length;
-          $scope.records = response.records;
-          $scope.markers = []
-          $scope.markers = _.map($scope.records,function(record) {
-            var fields = record.fields;
-            var record = { id: fields.CAMIS, coords: { longitude: fields.Longitude,latitude: fields.Latitude} };
-            return record;
-          });
-        },numberOfRestaurants,100,false);
-      });
-
-    });
-  }
-
-
-  $scope.map = {
-    control: {},
-    center: {
-      latitude: 40.7903,
-      longitude: -73.9597
-      },
-    zoom: 10,
-    refresh: function() {
-      return $scope.markers.length != $scope.markersPrevLength;
-    }
+function initialize() {
+  var mapOptions = {
+    center: { lat: -34.397, lng: 150.644},
+    zoom: 8
   };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
 
-}]);
 
-app.controller('markerController',['$scope',function($scope) {
-
-}]);
