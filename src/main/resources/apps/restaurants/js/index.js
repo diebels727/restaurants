53af33bc40ece521fddda6897a9119f39808cf66
diebels727 <diebelsalternative@hotmail.com>
2015-06-30 18:41:30 -0400

    Show random markers.

--- src/main/resources/apps/restaurants/js/index.js
+++ src/main/resources/apps/restaurants/js/index.js
@@ -49,11 +49,17 @@ $(function() {
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 
+    var markers = [];
+    for (var i = 0; i < 10000; i++) {
+      var center = map.getCenter()
 
-    var marker = new google.maps.Marker({
-      position: map.getCenter(),
-      map: map
-    });
+      var latLng = new google.maps.LatLng(center.lat() + Math.random(-0.01,0.01),
+          center.lng()+Math.random(-0.01,0.01));
+      var marker = new google.maps.Marker({'position': latLng});
+      markers.push(marker);
+    }
+
+    var mc = new MarkerClusterer(map,markers);
   }
 
   google.maps.event.addDomListener(window, 'load', initialize);
