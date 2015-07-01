94869db798548ecdde5c718bef65f5964c8547b3
diebels727 <diebelsalternative@hotmail.com>
2015-06-30 20:53:14 -0400

    Init with markers

--- src/main/resources/apps/restaurants/js/application.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('restApp',[]);
-
-app.controller('restAppController',['$scope',function($scope) {
-  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-}]);
--- src/main/resources/apps/restaurants/js/index.js
+++ src/main/resources/apps/restaurants/js/index.js
@@ -2,9 +2,11 @@ function enterDebug() {
   debugger;
 };
 
-$(function() {
-  function initialize() {
-    var mapOptions = {
+
+var app = angular.module('restApp',[])
+
+app.controller('applicationController',['$scope',function($scope) {
+    $scope.mapOptions = {
       center: {
          lat: 40.7903,
          lng: -73.9597
@@ -12,23 +14,19 @@ $(function() {
       zoom: 10
     };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+    $scope.map = new google.maps.Map(document.getElementById('map-canvas'),$scope.mapOptions);
 
-    var markers = [];
+    $scope.markers = [];
     for (var i = 0; i < 10000; i++) {
-      var center = map.getCenter()
+      var center = $scope.map.getCenter()
 
-      var latLng = new google.maps.LatLng(center.lat() + Math.random(-0.01,0.01),
-          center.lng()+Math.random(-0.01,0.01));
+      var latLng = new google.maps.LatLng(center.lat() + Math.random(-0.01,0.01),center.lng()+Math.random(-0.01,0.01));
       var marker = new google.maps.Marker({'position': latLng});
-      markers.push(marker);
+      $scope.markers.push(marker);
     }
 
-    var mc = new MarkerClusterer(map,markers);
-  }
-
-  google.maps.event.addDomListener(window, 'load', initialize);
-});
+    $scope.mc = new MarkerClusterer($scope.map,$scope.markers);
+    google.maps.event.addDomListener(window, 'load');
+}]);
 
 
