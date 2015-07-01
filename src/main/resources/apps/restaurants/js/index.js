646b54ebbf8e6488184c542c403266aad74fc55e
diebels727 <diebelsalternative@hotmail.com>
2015-06-30 22:06:41 -0400

    Will load violators.

--- src/main/resources/apps/restaurants/js/index.js
+++ src/main/resources/apps/restaurants/js/index.js
@@ -14,32 +14,48 @@ app.controller('applicationController',['$scope',function($scope) {
     $scope.map = new google.maps.Map(document.getElementById('map-canvas'),$scope.mapOptions);
     $scope.markers = [];
     $scope.koverseIsReady = KoverseApplication.EVENT_READY;
+    $scope.dataCollection = null;
 
-    Koverse.addEventListener($scope.koverseIsReady, function() {
-      console.log("[Koverse] Ready1.");
-      Koverse.getDataCollectionByName('violators',function(dataCollectionResponse) {
-        $scope.dataCollection = dataCollectionResponse.dataCollection;
-      });
-    });
+    // Koverse.addEventListener($scope.koverseIsReady, function() {
+      // console.log("[Koverse] Ready1.");
+      // Koverse.getDataCollectionByName('violators',function(dataCollectionResponse) {
+      //   $scope.dataCollection = dataCollectionResponse.dataCollection;
+      // });
+    // });
 
     Koverse.addEventListener($scope.koverseIsReady, function() {
       console.log("[Koverse] Ready2.");
-      Koverse.performQuery("{$any: *}",[$scope.dataCollection],function(response) {
-        $scope.records = response.records;
-        _.each($scope.records,function(record) {
-          var fields = record.fields;
-          var latLng = new google.maps.LatLng(fields.Latitude,fields.Longitude)
-          var marker = new google.maps.Marker({
-            'position': latLng
+
+      Koverse.getDataCollectionByName('violators',function(dataCollectionResponse) {
+        $scope.dataCollection = dataCollectionResponse.dataCollection;
+        Koverse.performQuery("{$any: *}",[$scope.dataCollection.id],function(response) {
+          $scope.records = response.records;
+          //var record = $scope.records.pop();
+
+          _.each($scope.records,function(record) {
+            //var restaurant = record.fields.restaurant.pop()
+            var fields = record.fields;
+            var latLng = new google.maps.LatLng(fields.Latitude,fields.Longitude)
+            var marker = new google.maps.Marker({
+              'position': latLng
+            });
+            console.log("[Koverse] Will push marker.");
+            $scope.markers.push(marker);
           });
-          console.log("[Koverse] Will push marker.");
-          $scope.markers.push(marker);
-        });
-        $scope.mc = new MarkerClusterer($scope.map,$scope.markers);
-      },1000,0,false);
+          $scope.mc = new MarkerClusterer($scope.map,$scope.markers);
+        },2,0,false);
+      });
+
+
 
     });
 
+    Koverse.addEventListener($scope.koverseIsReady, function() {
+      console.log("[Koverse] Ready3.");
+      $scope.mc = new MarkerClusterer($scope.map,$scope.markers);
+    });
+
+    $scope.mc = new MarkerClusterer($scope.map,$scope.markers);
     google.maps.event.addDomListener(window, 'load');
 
     $scope.debug = function() {
